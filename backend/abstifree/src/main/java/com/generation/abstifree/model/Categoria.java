85ca7538cbdcdf1826bec4eb4f19fffb09c367ec
Bruno Franchi <brunos.franchi@gmail.com>
2024-03-15 13:49:49 -0300

    Correção de erros no objeto Category.

--- backend/abstifree/src/main/java/com/generation/abstifree/model/Categoria.java
+++ backend/abstifree/src/main/java/com/generation/abstifree/model/Categoria.java
@@ -8,47 +8,44 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "tb_categorias")
 public class Categoria {
 
-	@Entity
-	@Table(name = "tb_categorias")
-	public class Theme {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
+	@NotNull(message = "O atributo nome categoria é obrigatório.")
+	@Size(min = 5, max = 50, message = "O atributo nome deve conter no minimo 05 e no máximo 50 caracteres")
+	private String nomeCategoria;
 
-		@NotNull(message = "O atributo nome_categoria é obrigatório.")
-		@Size(min = 5, max = 50, message = "O atributo nome deve conter no minimo 05 e no máximo 50 caracteres")
-		private String nome_categoria;
+	@NotNull(message = "O atributo descrição é obrigatório.")
+	@Size(min = 15, max = 150, message = "O atributo descrição deve conter no minimo 15 e no máximo 1000 caracteres")
+	private String descricaoCategoria;
 
-		@NotNull(message = "O atributo descrição é obrigatório.")
-		@Size(min = 15, max = 150, message = "O atributo descrição deve conter no minimo 15 e no máximo 1000 caracteres")
-		private String descricao;
-
-		public Long getId() {
-			return id;
-		}
-
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-		public String getNome_categoria() {
-			return nome_categoria;
-		}
-
-		public void setNome_categoria(String nome_categoria) {
-			this.nome_categoria = nome_categoria;
-		}
-
-		public String getDescricao() {
-			return descricao;
-		}
-
-		public void setDescricao(String descricao) {
-			this.descricao = descricao;
-		}
-
+	public Long getId() {
+		return id;
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome_categoria() {
+		return nomeCategoria;
+	}
+
+	public void setNome_categoria(String nome_categoria) {
+		this.nomeCategoria = nome_categoria;
+	}
+
+	public String getDescricao() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricaoCategoria = descricao;
+	}
+
 }
