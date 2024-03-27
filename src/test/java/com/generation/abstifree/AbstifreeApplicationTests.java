cc31d0b7744c5a8d2ed3723db207653f1c925475
Rodolfo Ocanha <157258616+RodolfoOcanha@users.noreply.github.com>
2024-03-27 12:22:46 -0300

    teste

---
+++ src/test/java/com/generation/abstifree/AbstifreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.abstifree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstifreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
